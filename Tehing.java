7f4ec27932bbe7cea055884588558aa445c712d6
KelderAnd <146181014+KelderAnd@users.noreply.github.com>
2024-03-18 21:55:50 +0200

    Create Tehing.java

---
+++ Tehing.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+
+/***************************************
+ *
+ *
+ * Kirjeldus: Klass TehingAndmed sisaldab andmeid tehinguid tegevate osapoolte vahel, nagu saatja, saaja, selgitus, summa, kuupäev.
+ * Lisaks sisaldab klass vajalikke gettereid.
+ *
+ * Autor: Andreas Kelder
+ *
+ **************************************/
+
+// Tehingu objekti klass
+public class Tehing {
+    private static String saatja;
+    private static String saaja;
+    private static String selgitus;
+    private static double summa;
+    private static Date kuupäev;
+
+    public Tehing(String saatja, String saaja, String selgitus, double summa, Date kuupäev) {
+        this.saatja = saatja;
+        this.saaja = saaja;
+        this.selgitus = selgitus;
+        this.summa = summa;
+        this.kuupäev = kuupäev;
+    }
+
+    public static String getSaatja() {
+        return saatja;
+    }
+
+    public static String getSaaja() {
+        return saaja;
+    }
+
+    public static String getSelgitus() {
+        return selgitus;
+    }
+
+    public static double getSumma() {
+        return summa;
+    }
+
+    public static Date getKuupäev() {
+        return kuupäev;
+    }
+}
