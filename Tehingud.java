2c48af7457137733f5e1190cab4c046c6d3bf76f
KelderAnd <146181014+KelderAnd@users.noreply.github.com>
2024-03-24 00:13:37 +0200

    Update Tehingud.java

--- Tehingud.java
+++ Tehingud.java
@@ -1,11 +1,12 @@
 package Rühmatöö;
 
 import java.io.*;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static Rühmatöö.Tehing.saatja;
 
 /***********************************
  *
@@ -19,54 +20,64 @@ import java.util.Comparator;
 
 public class Tehingud {
 
-    private ArrayList<Tehing> tehingud;
+    private static ArrayList<Tehing> tehingud;
     public Tehingud() {
-        this.tehingud = new ArrayList<>();
+        tehingud = new ArrayList<>();
     }
+
     // Meetod uue tehingu lisamiseks ajalukku
-    public void lisaTehing(String kasutajaNimi, String saaja, String selgitus, double summa, LocalDateTime kuupäev) {
-        Tehing tehing = new Tehing(kasutajaNimi, saaja, selgitus, summa, kuupäev);
+    public static void lisaTehing(String kasutajaNimi, String saaja, String selgitus, double summa, LocalDateTime kuupäev) { //Paul: Võiks kasutada lihtsalt Tehingu objekti parameetrina.
+        Tehing tehing = new Tehing(kasutajaNimi, saaja, selgitus, summa, kuupäev); // Paul: Siis lisada faili tehingu toString() Tulemus.
         tehingud.add(tehing);
     }
+
+    public static ArrayList<Tehing> getKasutajaTehingud(String kasutajanimi) {
+        ArrayList<Tehing> kasutajaTehingud = new ArrayList<>();
+        for (Tehing tehing : tehingud) {
+            if (tehing.getSaatja().equals(kasutajanimi)) {
+                kasutajaTehingud.add(tehing);
+            }
+        }
+        return kasutajaTehingud;
+    }
+
     // Meetod tehingute saamiseks
     public ArrayList<Tehing> getTehinguteAjalugu() {
         return tehingud;
     }
+
     // Tehingute ajaloo kuvamise funktsioon
     public void kuvaTehinguteAjalugu() {
         for (Tehing tehing : tehingud) {
+            //Paul: Siia lihtsalt toString meetodi väljakutse. 1 reaga.
             System.out.println("** Tehingu kirjeldus **");
             System.out.println("Saatja: " + Konto.getKasutajaNimi());
-            System.out.println("Saaja: " + tehing.getSaaja());
-            System.out.println("Selgitus: " + tehing.getSelgitus());
-            System.out.println("Summa: " + tehing.getSumma());
-            System.out.println("Kuupäev: " + tehing.getKuupäev());
+            System.out.println("Saaja: " + Tehing.getSaaja());
+            System.out.println("Selgitus: " + Tehing.getSelgitus());
+            System.out.println("Summa: " + Tehing.getSumma());
+            System.out.println("Kuupäev: " + Tehing.getKuupäev());
             System.out.println();
 
         }
     }
     // Meetod tehingute faili kirjutamiseks (Kui tahab)
-    public void kirjutaFaili(String failinimi){
+    public static void kirjutaFaili(String failinimi){
         try {
             FileWriter fileWriter = new FileWriter(new File(failinimi), true);
             PrintWriter printWriter = new PrintWriter(fileWriter);
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-            Set<String> unikaalsedTehingud = new HashSet<>();
 
             //Tehingu info faili salvestamine
             for (Tehing tehing: tehingud) {
-                String tehinguInfo = "Saatja: " + Konto.getKasutajaNimi() +
-                        "\nSaaja: " + tehing.getSaaja() +
-                        "\nSelgitus: " + tehing.getSelgitus() +
-                        "\nSumma: " + tehing.getSumma() +
-                        "\nKuupäev: " + formatter.format(tehing.getKuupäev());
-                if (unikaalsedTehingud.add(tehinguInfo)) {
-                    printWriter.println("** Tehingu Kirjeldus **");
-                    printWriter.println(tehinguInfo);
-                    printWriter.println();
-                }
+                //Paul: Siia samuti 1 reaga tostring väljakutse.
+                printWriter.println("** Tehingu kirjeldus **");
+                printWriter.println("Saatja: " + Konto.getKasutajaNimi());
+                printWriter.println("Saaja: " + tehing.getSaaja());
+                printWriter.println("Selgitus: " + tehing.getSelgitus());
+                printWriter.println("Summa: " + tehing.getSumma());
+                printWriter.println("Kuupäev: " + formatter.format(tehing.getKuupäev()));
+                printWriter.println();
             }
-            sorteeriTehingudKuupäevaJärgi(tehingud);
             printWriter.close();
             System.out.println("Tehingu info on salvestatud faili " + failinimi);
         } catch (IOException e) {
@@ -74,7 +85,7 @@ public class Tehingud {
         }
     }
     // Meetod tehingute failist lugemiseks (Kui tahta)
-    public void loeFaili(String failinimi) {
+    public static void loeFaili(String failinimi) {
         try {
             File file = new File(failinimi);
             BufferedReader reader = new BufferedReader(new FileReader(failinimi));
@@ -82,19 +93,23 @@ public class Tehingud {
             ArrayList<Tehing> tehingud = new ArrayList<>();
 
             while ((line = reader.readLine()) != null) {
-                // Loetakse failist iga tehingu kirjeldus
-                String kasutajaNimi = reader.readLine().substring(8); // "Saatja: " jäetakse välja
-                String saaja = reader.readLine().substring(7); // "Saaja: " jäetakse välja
-                String selgitus = reader.readLine().substring(10); // "Selgitus: " jäetakse välja
-                double summa = Double.parseDouble(reader.readLine().substring(7)); // "Summa: " jäetakse välja
-                LocalDateTime kuupäev = LocalDateTime.parse(reader.readLine().substring(9), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")); // "Kuupäev: " jäetakse välja
-                reader.readLine(); // Tühirida
+                if (line.equals("** Tehingu kirjeldus **")) {
+                    // Loetakse failist iga tehingu kirjeldus
+                    String saatja = reader.readLine().substring(8); // "Saatja: " jäetakse välja
+                    String saaja = reader.readLine().substring(7); // "Saaja: " jäetakse välja
+                    String selgitus = reader.readLine().substring(10); // "Selgitus: " jäetakse välja
+                    double summa = Double.parseDouble(reader.readLine().substring(7)); // "Summa: " jäetakse välja
+                    LocalDateTime kuupäev = LocalDateTime.parse(reader.readLine().substring(9), DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")); // "Kuupäev: " jäetakse välja
+                    reader.readLine(); // Tühirida
 
-                // Lisatakse tehing tehingute listi
-                tehingud.add(new Tehing(kasutajaNimi, saaja, selgitus, summa, kuupäev));
+                    // Lisatakse tehing tehingute listi
+                    tehingud.add(new Tehing(saatja, saaja, selgitus, summa, kuupäev));
+                }
             }
             reader.close();
-            tehingud.sort(Comparator.comparing(Tehing::getKuupäev));
+
+            // Sorteeritakse tehingud kuupäeva järgi
+            Collections.sort(tehingud, (t1, t2) -> t2.getKuupäev().compareTo(t1.getKuupäev()));
 
             // Väljastatakse tehingud
             for (Tehing tehing : tehingud) {
@@ -107,21 +122,20 @@ public class Tehingud {
                 System.out.println();
             }
         } catch (FileNotFoundException e) {
+            System.out.println("Faili ei leitud. ");
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public void sorteeriTehingudKuupäevaJärgi(ArrayList<Tehing> tehingud) {
-        LocalDateTime praeguneHetk = LocalDateTime.now();
-        this.tehingud.sort(Comparator.comparingLong(t -> Duration.between(t.getKuupäev(), praeguneHetk).toMillis()));
-    /*public void sorteeriTehingudKuupäevaJärgi() {
-        Collections.sort(tehingud, new Comparator<Tehing>() {
-            @Override
-            public int compare(Tehing t1, Tehing t2) {
-                return t2.getKuupäev().compareTo(t1.getKuupäev());
+    public static String kuvaTehingudKasutajanimega(String kasutajanimi){
+        StringBuilder sb = new StringBuilder();
+        for(Tehing tehing : tehingud){
+            if (Konto.getKasutajaNimi().equals(kasutajanimi)){
+                sb.append(tehing).append("\n");
             }
-        });
-    */}
+        }
+        return sb.toString();
+    }
 
 }
